5f00fb0ebe1fa30b5a25648117a4c085d595d650
Ann Gentile <gentile@sandia.gov>
2024-12-31 15:53:24 -0700

    make the query like a service (like the oracle)

---
+++ ldms/src/sampler/dynamic_stream/dynamic_query_client.c
@@ -0,0 +1,159 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2024 National Technology & Engineering Solutions
+ * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
+ * NTESS, the U.S. Government retains certain rights in this software.
+ * Copyright (c) 2024 Open Grid Computing, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "dynamic_query.h"
+
+static struct sockaddr_in stSockAddr;
+static int Res = -1;
+static int SocketFD = -1;
+
+void cleanup(){
+
+}
+
+void signal_handler(int signum){
+        printf("In signal handler\n");
+        cleanup();
+        exit(signum);
+}
+
+static int setupSocket(){
+        struct hostent *he;
+        struct in_addr **addr_list;
+
+
+        if ((he = gethostbyname("localhost")) == NULL) {  // get the host info
+                printf("Error: failed gethostbyname(%s)\n", "localhost");
+                return -1;
+        }
+
+        addr_list = (struct in_addr **)he->h_addr_list;
+        if (addr_list[0] == NULL) {
+                printf("Error: failed addr_list[0]\n");
+                return -1;
+        }
+
+        SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        if (-1 == SocketFD) {
+                printf("Error: cannot create socket");
+                return -1;
+
+        }
+
+        memset(&stSockAddr, 0, sizeof(stSockAddr));
+
+        stSockAddr.sin_family = AF_INET;
+        stSockAddr.sin_port = htons(DYNAMIC_SERVICE_PORT);
+        Res = inet_pton(AF_INET, inet_ntoa(*addr_list[0]), &stSockAddr.sin_addr);
+
+        if (0 > Res) {
+                printf("error: first parameter is not a valid address family");
+                close(SocketFD);
+                SocketFD = -1;
+                return -1;
+        }  else if (0 == Res) {
+                printf("char string (second parameter does not contain valid ipaddress)");
+                close(SocketFD);
+                SocketFD = -1;
+                return -1;
+        }
+
+        if (-1 == connect(SocketFD, (struct sockaddr *)&stSockAddr, sizeof(stSockAddr))) {
+                printf("connect failed");
+                close(SocketFD);
+                SocketFD = -1;
+                return -1;
+        }
+
+        return 0;
+
+}
+
+
+static int makeQuery(char* qu){
+
+        char sendBuff[MAX_MBUF];
+        int rc;
+
+        if (SocketFD == -1){
+                return -1;
+        }
+
+        snprintf(sendBuff, MAX_MBUF, "%s", qu);
+
+        /* perform write operations ... */
+        printf("Sending %s\n", sendBuff);
+        rc = write(SocketFD, sendBuff, strlen(sendBuff));
+
+        return 0;
+};
+
+
+int main(int argc, char **argv){
+
+        int rc;
+
+        signal(SIGCHLD, SIG_IGN);
+        signal(SIGINT, signal_handler);
+        signal(SIGHUP, signal_handler);
+
+        if (argc != 2){
+                printf("Usage ./dynamic_query_client <QUERY_1>\n");
+                exit (-1);
+        }
+
+        rc = setupSocket();
+        if (rc){
+                printf("can't setup socket\n");
+                exit(-1);
+        }
+
+        rc = makeQuery(argv[1]);
+        return rc;
+}
---
+++ ldms/src/sampler/dynamic_stream/dynamic_query_service.c
@@ -0,0 +1,378 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2024 National Technology & Engineering Solutions
+ * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
+ * NTESS, the U.S. Government retains certain rights in this software.
+ * Copyright (c) 2024 Open Grid Computing, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "dynamic_query.h"
+#include <ovis_json/ovis_json.h>
+#include <ovis_util/util.h>
+#include "ldms.h"
+#include "ldmsd.h"
+#include "ldmsd_stream.h"
+
+static char *host = NULL;
+static char *port = NULL;
+static char *xprt = "sock";
+static char *auth = "none";
+static char *stream = NULL;
+static const int auth_opt_max = AUTH_OPT_MAX;
+static ldmsd_stream_type_t typ = LDMSD_STREAM_JSON;
+static ldms_t ldms = NULL;
+static int SocketFD = -1;
+static int ConnectFD = -1;
+
+
+static struct option long_opts[] = {
+	{"host",     required_argument, 0,  'h' },
+	{"port",     required_argument, 0,  'p' },
+	{"stream",   required_argument, 0,  's' },
+	{"xprt",     required_argument, 0,  'x' },
+	{"auth",     required_argument, 0,  'a' },
+	{"auth_arg", required_argument, 0,  'A' },
+	{0,          0,                 0,  0 }
+};
+
+void usage(int argc, char **argv) __attribute__((noreturn));
+void usage(int argc, char **argv){
+	printf("usage: %s -x <xprt> -h <host> -p <port> "
+	       "-s <stream-name> "
+	       "-a <auth> -A <auth-opt> \n",
+	       argv[0]);
+	exit(1);
+}
+
+static const char *short_opts = "h:p:s:x:a:A:";
+
+
+void cleanup(){
+
+        ldms_xprt_close(ldms);
+}
+
+
+void signal_handler(int signum){
+        printf("In signal handler\n");
+        cleanup();
+        exit(signum);
+}
+
+
+jbuf_t execResultsQuery(int qu){
+
+        int rc = 0;
+        int len;
+        jbuf_t jb;
+        char s[100] = "This is the result";
+
+        // this will execute a query on the database (in another thread?)
+        printf("Should be doing query %d '%s'\n", qu, queries[qu].qstring);
+
+        // will need to know how to match up queries to generate
+        // values and queries to get their results
+        // should this be a callback on the result being obtained?
+
+        len = strlen(s);
+        //Get rid of trailing whitespace and newlines
+        while (len &&
+               (isspace(s[len-1]) || s[len-1] == '\n')) {
+                len--;
+        }
+        s[len] = '\0';
+
+        printf("Should be building the jbuf\n");
+
+        jb = jbuf_new();
+        if (!jb) goto out;
+        jb = jbuf_append_str(jb, "{");
+        if (!jb) goto out;
+        jb = jbuf_append_attr(jb, MSG_KEY, "\"%s\"", s);
+        if (!jb) goto out;
+        jb = jbuf_append_str(jb, "}}");
+        if (!jb) goto out;
+
+        printf("Should be publishing '%s'\n", jb->buf);
+
+ out:
+
+        return jb;
+
+}
+
+int parseQuery(char* msg_buf){
+        int qu;
+        int k;
+
+        for (k = 0; k < NUM_QUERIES; k++){
+                if (!strcmp(queries[k].qkey, msg_buf)){
+                        qu = k;
+                        break;
+                }
+        }
+        if (qu == -1){
+                printf("The query string is invalid '%s'\n",
+                       msg_buf);
+        }
+
+        return qu;
+}
+
+
+int parseArgs(int argc, char **argv){
+        int opt, opt_idx;
+        char *lval, *rval;
+        struct attr_value_list *auth_opt = NULL;
+
+        int rc;
+
+        auth_opt = av_new(auth_opt_max);
+        if (!auth_opt) {
+                perror("could not allocate auth options");
+                exit(1);
+        }
+
+        while ((opt = getopt_long(argc, argv,
+                                  short_opts, long_opts,
+                                  &opt_idx)) > 0) {
+                switch (opt) {
+                case 'h':
+                        host = strdup(optarg);
+                        if (!host) {
+                                printf("ERROR: out of memory\n");
+                                exit(1);
+                        }
+                        break;
+                case 'p':
+                        port = strdup(optarg);
+                        if (!port) {
+                                printf("ERROR: out of memory\n");
+                                exit(1);
+                        }
+                        break;
+		case 'x':
+                        xprt = strdup(optarg);
+			if (!xprt) {
+				printf("ERROR: out of memory\n");
+				exit(1);
+			}
+			break;
+		case 'a':
+			auth = strdup(optarg);
+			if (!auth) {
+				printf("ERROR: out of memory\n");
+				exit(1);
+			}
+			break;
+		case 'A':
+			lval = strtok(optarg, "=");
+			rval = strtok(NULL, "");
+			if (!lval || !rval) {
+				printf("ERROR: Expecting -A name=value");
+				exit(1);
+			}
+			if (auth_opt->count == auth_opt->size) {
+				printf("ERROR: Too many auth options");
+				exit(1);
+			}
+			auth_opt->list[auth_opt->count].name = lval;
+			auth_opt->list[auth_opt->count].value = rval;
+			auth_opt->count++;
+			break;
+		case 's':
+			stream = strdup(optarg);
+			if (!stream) {
+				printf("ERROR: out of memory\n");
+				exit(1);
+			}
+			break;
+		default:
+			usage(argc, argv);
+		}
+	}
+	if (!host || !port || !stream )
+		usage(argc, argv);
+
+        return 0;
+}
+
+
+int setupLDMSD(){
+
+	int rc;
+
+        ldms = ldms_xprt_new_with_auth(xprt, NULL, auth, NULL);
+        if (!ldms) {
+          rc = errno;
+          printf("Failed to create the LDMS transport endpoint.\n");
+          return rc;
+        }
+
+        rc = ldms_xprt_connect_by_name(ldms, host, port, NULL, NULL);
+        if (rc){
+          printf("Error %d connecting to peer\n", rc);
+          return rc;
+        }
+
+        return rc;
+}
+
+void handleMsg(int CFD){
+
+        char recvBuff[MAX_MBUF];
+        char tempBuff[MAX_MBUF];
+        char sendBuff[MAX_MBUF];
+
+        jbuf_t jb;
+	int numrcv;
+        int qu;
+	int rc;
+
+        memset(recvBuff, '0', sizeof(recvBuff));
+        numrcv = read(CFD, recvBuff, sizeof(recvBuff));
+	recvBuff[numrcv] = '\0';
+	printf("Received %s\n", recvBuff);
+
+
+	qu = parseQuery(recvBuff);
+        if (qu == -1){
+		printf("Warning: ignoring bad query\n");
+		return;
+	}
+
+        printf("Should be doing query %d '%s'\n", qu, queries[qu].qstring);
+
+        jb = execResultsQuery(qu);
+        if (jb == NULL){
+                printf("jb is null\n");
+                goto out;
+        }
+
+        rc = ldmsd_stream_publish(ldms, stream, typ,
+                                  jb->buf, jb->cursor+1);
+        if (rc) {
+                printf("Error on stream publish\n");
+               goto out;
+        }
+ out:
+
+        if (jb)
+                jbuf_free(jb);
+
+        return;
+}
+
+
+int main(int argc, char **argv){
+
+        int rc;
+
+        signal(SIGCHLD, SIG_IGN);
+        signal(SIGINT, signal_handler);
+        signal(SIGHUP, signal_handler);
+
+        rc = parseArgs(argc, argv);
+        if (rc != 0){
+                printf("Bad args\n");
+                exit(-1);
+        }
+
+
+        rc = setupLDMSD();
+        if (rc != 0){
+                printf("Cannot setup LDMSD\n");
+                exit(-1);
+        }
+
+	struct sockaddr_in stSockAddr;
+	SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (-1 == SocketFD) {
+                perror("can not create socket");
+                exit(EXIT_FAILURE);
+	}
+
+        memset(&stSockAddr, NULL, sizeof(stSockAddr));
+
+        stSockAddr.sin_family = AF_INET;
+        stSockAddr.sin_port = htons(DYNAMIC_SERVICE_PORT);
+	stSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        if (-1 == bind(SocketFD,(struct sockaddr *)&stSockAddr, sizeof(stSockAddr))) {
+                perror("error bind failed");
+                close(SocketFD);
+                exit(EXIT_FAILURE);
+        }
+
+        if (-1 == listen(SocketFD, DYNAMIC_SERVICE_LISTEN_BACKLOG)) {
+	        perror("error listen failed");
+		close(SocketFD);
+	        exit(EXIT_FAILURE);
+        }
+
+        for(;;) {
+                ConnectFD = accept(SocketFD, NULL, NULL);
+                if (ConnectFD < 0) {
+	                perror("error accept failed");
+                        close(SocketFD);
+	                exit(EXIT_FAILURE);
+                }
+
+                int pid = fork();
+		if (pid < 0)
+                        perror("error on fork");
+		if (pid == 0){
+	                close(SocketFD);
+                        handleMsg(ConnectFD);
+                        exit(0);
+                } else {
+                        close(ConnectFD);
+                }
+	}
+
+	close(SocketFD);
+
+ out:
+        cleanup();
+        return 0;
+}
